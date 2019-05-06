23833491acca6a2ccde7c523016c143750407766
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2019-05-06 22:15:49 +0200

    wip

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,24 +1,40 @@
-import * as React from "react"
-import request from 'request'
-
+import * as React from "react";
+import superagent from "superagent";
 
 class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    superagent
+      .get(
+        "https://gorest.co.in/public-api/users?_format=json&access-token=9N9PEdQelLM6JVDd2_5zbRWMV2BFP5XMNaz2"
+      )
+      .end((err, res) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(res);
+          this.setState({
+            users: res.body.result
+          });
+        }
+      });
+  }
 
-    componentDidMount () {
-        request("https://gorest.co.in/public-api/users?_format=json&access-token=9N9PEdQelLM6JVDd2_5zbRWMV2BFP5XMNaz2",
-            (error, response, body) => {
-                console.log('error', error);
-                console.log('statusCode', response);
-                console.log('body', body);
-               
-            })
-    }
-
-    render() {
-        return (
-            <div><h1>Liste users</h1></div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Liste users</h1>
+        {this.state.users.map((user) => {
+          return <div>{user.email}</div>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default Users;
