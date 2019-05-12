83979637d0121c0a3b8cf5404dbf0144cd562588
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2019-05-12 18:03:35 +0200

    wip

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,21 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-import About from "./pages/About"
+import About from "./pages/About";
 
 class RouterComponent extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Route path="/user" Component={() => <div>User</div>} />
-                <Route path="/about" Component={About} />
-                <Route exact path="/" component={() => <div>Home</div>} />
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/user" component={() => <div>User</div>} />
+          <Route path="/about" component={About} />
+          <Route exact path="/" component={() => <div>Home</div>} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default RouterComponent;
